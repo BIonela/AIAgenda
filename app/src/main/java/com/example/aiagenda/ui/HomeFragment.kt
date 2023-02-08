27e96b5539babaca32298c92ac2aeb9e83abdbc3
DESKTOP-14NFG4G\ionela <ionelamihaela003@gmail.com>
2023-02-08 12:05:22 +0200

    OnBackPressed when signing out fixed

--- app/src/main/java/com/example/aiagenda/ui/HomeFragment.kt
+++ app/src/main/java/com/example/aiagenda/ui/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -31,5 +33,10 @@ class HomeFragment : Fragment() {
             false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {}
     }
 }
